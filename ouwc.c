1849dc96e08fb62f48275084c71fb7e9435ed554
Zane Gray <zanejgr@gmail.com>
2019-02-23 05:23:45 +0000

    broken files

--- ouwc.c
+++ ouwc.c
@@ -1,12 +1,34 @@
-#ifdef DEBUG
-#include "Util.h"
-#endif
-
-
-#include <stdio.h>
-int main()
+#include "proj0.h"
+int main(int argc,char**argv)
 {
-	printf("placeholder");
-	return 0;
+	FILE*file;
+	//check that the arguments are valid
+	if(argc!=2&&argc!=3){
+		perror("usage: ouwc [-l|-c] <file>");
+		return EXIT_FAILURE;
+	}
+	//attempt to open the file, if any
+	if(argc==2) file = stdin;
+	if(argc==3&&!(file=fopen(argv[2],"r"))){
+		perror("usage: ouwc [-l|-c] <file>");
+		return EXIT_FAILURE;
+	}
+	//count lines or characters depending on argv[1]
+	int count = 0;	
+	char*strptr=NULL;
+	size_t foo = 0;
+	int numread = 0;
+	while(numread=getline(&strptr,&foo,file)!=-1){
+		if(!strncmp(argv[1],"-n",2)) count+=numread;
+		else if(!strncmp(argv[1],"-l",2)) count++;
+		else{
+			perror("usage: ouwc [-l|-c] <file>");
+			return EXIT_FAILURE;
+		}	
+
+	}
+	printf("%d",count);
+	return EXIT_SUCCESS;
+
 }
 
