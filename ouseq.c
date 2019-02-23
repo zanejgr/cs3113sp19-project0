364e54f9e4d0a8ffe26d05c857cdc670ff241186
Zane Gray <zanejgr@gmail.com>
2019-02-23 04:31:38 +0000

    working ouseq

--- ouseq.c
+++ ouseq.c
@@ -1,12 +1,39 @@
-#ifdef DEBUG
-#include "Util.h"
-#endif
-
-
-#include <stdio.h>
-int main()
+#include "proj0.h"
+int main(int argc,char**argv)
 {
-	printf("placeholder");
-	return 0;
+	int m,n;
+	//validate input first
+	if(argc!=3&&argc!=2){
+		fprintf(stderr,"usage: ouseq[M] N");
+		return EXIT_FAILURE;
+	}//m defaults to 0
+	if(argc==2){
+		m=0;
+		n=atoi(argv[1]);
+		if(n==0&&argv[1][sizeof argv[1]-1]!='0'){
+
+			fprintf(stderr,"usage: ouseq[M] N");
+			return EXIT_FAILURE;
+		}
+	}else{
+		m=atoi(argv[1]);
+		n=atoi(argv[2]);
+		if(m==0&&argv[1][sizeof argv[1]-1]!='0'||
+				n==0&&argv[2][sizeof argv[2]-1]!='0'){
+			fprintf(stderr,"usage: ouseq[M] N");
+			return EXIT_FAILURE;
+		}
+	}
+	//special case:m=n
+	if(m==n){
+		printf("%d",m);
+		return EXIT_SUCCESS;
+	}
+	
+	int incr = m<n?1:-1;
+	for(int i = m;i!=n+incr;i+=incr){
+		printf("%d\n",i);
+	}
+	return EXIT_SUCCESS;
 }
 
